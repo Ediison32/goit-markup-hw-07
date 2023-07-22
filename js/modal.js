b34cd5d8ba29249fab085b0d8e239d5581c01b01
Ediison32 <edisonalvarez32@gmail.com>
2023-07-21 19:23:25 -0500

    v-0-2 organizar modal

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector(".data-modal-open"),
     closeModalBtn: document.querySelector(".data-modal-close"), 
     modal: document.querySelector(".data-modal"),
+    modalSection: document.querySelector(".modal-section")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -11,5 +12,6 @@
   function toggleModal() {
     /* refs.modal.classList.toggle("is-hidden"); */
     refs.modal.classList.toggle("modal");
+    refs.modalSection.classList.toggle("modal2")
   }
 })();
